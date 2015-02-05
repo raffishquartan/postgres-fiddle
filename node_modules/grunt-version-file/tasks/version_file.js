6f5540e933a44eb2b04bea17e7d72f0a7b00d288
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-05 12:10:49 +0000

    node_modules directory for Grunt

---
+++ node_modules/grunt-version-file/tasks/version_file.js
@@ -0,0 +1,12 @@
+/*
+ * grunt-version-file
+ * https://github.com/raffishquartan/grunt-version-file
+ *
+ * Copyright (c) 2014 raffishquartan
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var grunt_version_file_task = require("../lib/grunt_version_file");
+module.exports = grunt_version_file_task.register_with_grunt;
