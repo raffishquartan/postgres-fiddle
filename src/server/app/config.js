3fefcc75a00cbbe83fc26fdb7b54b67232a1b0ef
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-05 20:10:06 +0000

    src/server/**/*.js - jshint fixes

--- src/server/app/config.js
+++ src/server/app/config.js
@@ -1,12 +1,11 @@
-"use strict";
+'use strict';
 
-var path = require("path");
+var path = require('path');
 
 module.exports = {
   http_port: 27973,
-  database_string: "postgres://postgres_fiddle:postgres_fiddle@localhost:5432/postgres_fiddle",
+  database_string: 'postgres://postgres_fiddle:postgres_fiddle@localhost:5432/postgres_fiddle',
   server_root: __dirname,
-  client_root: path.join(__dirname, "../client"),
-  express_logger_format:
-    ":remote-addr - - [:date] \":method :url HTTP/:http-version\" :status \":referrer\" \":user-agent\"",
+  client_root: path.join(__dirname, '../client'),
+  express_logger_format: ':remote-addr - [:date] ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"',
 };
