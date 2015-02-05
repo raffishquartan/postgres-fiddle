0c55c7f64b01af75dce5271b28b3815d85ded65d
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-05 19:33:23 +0000

    api/entry/router.js - stub impl

---
+++ src/server/app/api/entry/router.js
@@ -0,0 +1,9 @@
+function get_entry_json(req, res) {
+  res.status(200).send('{ foo: "bar" }');
+}
+
+var express = require('express');
+var router = new express.Router();
+router.get('/:tag_string?', get_entry_json)
+
+module.exports = router;
