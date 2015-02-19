3770835f43e9c8c484ed7061d14776262b956ae8
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-19 16:26:05 +0000

    js/require-config.js - fixed paths to bower components

--- src/client/js/require-config.js
+++ src/client/js/require-config.js
@@ -17,12 +17,12 @@ require.config({
     }
   },
   paths: {
-    backbone: '../bower_components/backbone/backbone',
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
-    jquery: '../bower_components/jquery/dist/jquery',
-    marionette: '../bower_components/marionette/lib/backbone.marionette',
-    text: '../bower_components/requirejs-text/text',
-    underscore: '../bower_components/underscore/underscore'
+    backbone: '/bower_components/backbone/backbone',
+    bootstrap: '/bower_components/bootstrap/dist/js/bootstrap',
+    jquery: '/bower_components/jquery/dist/jquery',
+    marionette: '/bower_components/marionette/lib/backbone.marionette',
+    text: '/bower_components/requirejs-text/text',
+    underscore: '/bower_components/underscore/underscore'
   },
   urlArgs: 'bust=' + (new Date()).getTime() // development only
 });
