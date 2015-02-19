f78d31cae74278443038a2a74380a7659e941da8
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-19 16:10:07 +0000

    src/client/bower.json, src/client/js/require-config.js - requirejs-text added, " to ' in strings

--- src/client/js/require-config.js
+++ src/client/js/require-config.js
@@ -1,27 +1,28 @@
 require.config({
   shim: {
     bootstrap: {
-      deps: ["jquery"],
-      exports: "$.fn.popover"
+      deps: ['jquery'],
+      exports: '$.fn.popover'
     },
     backbone: {
-      deps: ["underscore", "jquery"],
-      exports: "Backbone"
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
     },
     marionette: {
-      deps: ["jquery", "underscore", "backbone"],
-      exports: "Marionette"
+      deps: ['jquery', 'underscore', 'backbone'],
+      exports: 'Marionette'
     },
     underscore: {
-      exports: "_"
+      exports: '_'
     }
   },
   paths: {
-    backbone: "../bower_components/backbone/backbone",
-    bootstrap: "../bower_components/bootstrap/dist/js/bootstrap",
-    jquery: "../bower_components/jquery/dist/jquery",
-    marionette: "../bower_components/marionette/lib/backbone.marionette",
-    underscore: "../bower_components/underscore/underscore"
+    backbone: '../bower_components/backbone/backbone',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+    jquery: '../bower_components/jquery/dist/jquery',
+    marionette: '../bower_components/marionette/lib/backbone.marionette',
+    text: '../bower_components/requirejs-text/text',
+    underscore: '../bower_components/underscore/underscore'
   },
-  urlArgs: "bust=" + (new Date()).getTime() // development only
+  urlArgs: 'bust=' + (new Date()).getTime() // development only
 });
