1d8644c2af0eb457993a0db13d0b4e7b9662fd63
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-19 15:57:28 +0000

    src/client/js/common/views.js - PF specific intermediates for easy Marionette base view extension

---
+++ src/client/js/common/views.js
@@ -0,0 +1,13 @@
+define(function(require) {
+  var PF = require('js/app-obj');
+
+  PF.module('Common.Views', function(Views, PF, Backbone, Marionette, $, _) {
+    Views.PFItemView = Marionette.ItemView.extend();
+
+    Views.PFCollectionView = Marionette.CollectionView.extend();
+
+    Views.PFCompositeView = Marionette.CompositeView.extend();
+
+    Views.PFLayout = Marionette.Layout.extend();
+  });
+});
