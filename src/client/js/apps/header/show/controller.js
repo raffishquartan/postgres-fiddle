413d201b4c11a078c0147fbe4acf27bbda3e170d
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-02-28 11:49:39 +0000

    js/apps/header - initial version generating menu from models

--- src/client/js/apps/header/show/controller.js
+++ src/client/js/apps/header/show/controller.js
@@ -1,22 +1,24 @@
 define(function(require) {
   var PF = require('js/app/obj');
   var logger = PF.logger.get_logger('root/js/apps/header/show/controller');
-  logger.trace('require:lambda - enter');
+  logger.trace('require:lambda -- enter');
 
   PF.module('HeaderApp.Show', function(Show, PF, Backbone, Marionette, $, _) {
-    logger.trace('PF.module:HeaderApp.Show - enter');
+    logger.trace('PF.module -- enter');
     Show.controller = {
       show_header: function() {
-        logger.trace('HeaderApp.Show.controller.show_header - enter');
+        logger.trace('HeaderApp.Show.controller.show_header -- enter');
+        require('js/apps/header/entities');
         var Views = require('js/apps/header/show/views');
-        var view = new Views.Header();
+        var navitem_collection = PF.request('headerapp:entities:navitems');
+        var view = new Views.Header({ collection: navitem_collection });
         PF.region_navbar.show(view);
-        logger.trace('HeaderApp.Show.controller.show_header - exit');
+        logger.trace('HeaderApp.Show.controller.show_header -- exit');
       }
     };
-    logger.trace('PF.module:HeaderApp.Show - exit');
+    logger.trace('PF.module -- exit');
   });
 
-  logger.trace('require:lambda - exit');
+  logger.trace('require:lambda -- exit');
   return PF.HeaderApp.Show.controller;
 });
--- src/client/js/apps/header/show/views.js
+++ src/client/js/apps/header/show/views.js
@@ -1,18 +1,26 @@
 define(function(require) {
   var PF = require('js/app/obj');
   var logger = PF.logger.get_logger('root/js/apps/header/show/view');
-  logger.trace('require:lambda - enter');
+  logger.trace('require:lambda -- enter');
 
   PF.module('HeaderApp.Show.Views', function(Views, PF, Backbone, Marionette, $, _) {
-    logger.trace('PF.module:HeaderApp.Show.Views - enter');
+    logger.trace('PF.module -- enter');
     require('js/common/views');
 
-    Views.Header = PF.Common.Views.PFItemView.extend({
-      template: _.template(require('text!js/apps/header/show/templates/header.html')),
+    Views.NavItemView = PF.Common.Views.PFItemView.extend({
+      template: _.template(require('text!js/apps/header/show/templates/navitem.html')),
+      tagName: 'li'
     });
-    logger.trace('PF.module:HeaderApp.Show.Views - exit');
+
+    Views.Header = PF.Common.Views.PFCompositeView.extend({
+      template: _.template(require('text!js/apps/header/show/templates/header.html')),
+      childView: Views.NavItemView,
+      childViewContainer: 'ul.js-navbar-items'
+    });
+
+    logger.trace('PF.module -- exit');
   });
 
-  logger.trace('require:lambda - exit');
+  logger.trace('require:lambda -- exit');
   return PF.HeaderApp.Show.Views;
 });
