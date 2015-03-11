963ee81867c9fdfcf5f93cbb01671c3b44f7076e
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-03-11 08:41:57 +0000

    server - refactor logger module, config module refs to be non-capitalised (those are only for newable objects)

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -10,9 +10,9 @@ var errorhandler = require('errorhandler');
 var log4js = require('log4js');
 var path = require('path');
 
-var C = require('app/config');
-var L = require('app/util/logger');
-var logger = L.get_logger('app/server');
+var server_config = require('app/config/server');
+var logger_module = require('app/util/logger');
+var logger = logger_module.get_logger('app/server');
 
 process.on('uncaughtException', function(error) {
   if(logger) {
@@ -29,8 +29,8 @@ Error.stackTraceLimit = Infinity;
 var app = express();
 configure_express_middleware(app);
 var http_server = http.createServer(app);
-http_server.listen(C.http_port, function() {
-  logger.info('Express HTTP server listening on port ' + C.http_port);
+http_server.listen(server_config.http_port, function() {
+  logger.info('Express HTTP server listening on port ' + server_config.http_port);
 });
 
 
@@ -39,8 +39,8 @@ function configure_express_middleware(app) {
   var logger_config = require('app/config/logger');
 
   app.use(compression());
-  app.use(serve_favicon(path.join(C.client_root, 'assets', 'images', 'favicons', 'favicon.ico')));
-  app.use(log4js.connectLogger(L.get_log4js_logger('connect-appender'),
+  app.use(serve_favicon(path.join(server_config.client_root, 'assets', 'images', 'favicons', 'favicon.ico')));
+  app.use(log4js.connectLogger(logger_module.get_log4js_logger('connect-appender'),
     { level: 'auto', layout: 'basic', immediate: true, format: logger_config.express_format }));
   app.use(body_parser.json());
   app.use(body_parser.urlencoded({ extended: false }));
@@ -48,11 +48,11 @@ function configure_express_middleware(app) {
   app.use(method_override('X-HTTP-Method-Override')); // Google/GData
   app.use(method_override('X-Method-Override'));      // IBM
   app.use('/api', require('app/api/entry/router'));
-  app.use('/assets', express.static(path.join(C.client_root, 'assets')));
-  app.use('/bower_components', express.static(path.join(C.client_root, 'bower_components')));
-  app.use('/js', express.static(path.join(C.client_root, 'js')));
+  app.use('/assets', express.static(path.join(server_config.client_root, 'assets')));
+  app.use('/bower_components', express.static(path.join(server_config.client_root, 'bower_components')));
+  app.use('/js', express.static(path.join(server_config.client_root, 'js')));
   app.use(function(req, res, next) { // Fall back to always sending index.html
-    res.sendFile(path.join(C.client_root, 'index.html'));
+    res.sendFile(path.join(server_config.client_root, 'index.html'));
   });
   app.use(errorhandler()); // not appropriate for a prod site, see SES / http://calv.info/node-and-express-tips/ for another approach
 }
