76c4774211e5452f29de9ae4b56b79bf6600c32a
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-03-14 22:01:58 +0000

    app/util/pr, app/config/database, scripts/<lots> - using Sequelize ORM; schema pf -> pfsq; wins table and model defn

---
+++ src/server/app/util/pr/entry.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var models = {
+    entry: sequelize.define('entry', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        validate: {
+          isUUID: 4
+        },
+        primaryKey: true
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          notNull: true
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          notNull: true
+        }
+      }
+    }, {
+    }),
+
+    tag: sequelize.define('tag', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate: {
+          isUUID: 4
+        },
+        primaryKey: true
+      },
+      value: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: true
+        }
+      }
+    }, {
+    })
+  };
+
+  models.tag.belongsToMany(models.entry, { through: 'entry_tag' });
+  models.entry.belongsToMany(models.tag, { through: 'entry_tag' });
+
+  return models;
+};
