3e273a479c0b5066518c53151dda8614d81f8b40
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-03-19 08:34:27 +0000

    server/app/util/util_itest.js - first set of module exports API tests to make refactoring easier

---
+++ src/server/app/util/util_itest.js
@@ -0,0 +1,81 @@
+// Allow statements that are not assignments or function calls (e.g. should statements)
+/* jshint -W030 */
+
+'use strict';
+
+var should = require('should');
+var _ = require('underscore');
+
+// https://gist.github.com/kethinov/6658166 - requires a trailing slash on the dir argument
+var walk_sync_path = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walk_sync_path(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push(dir + file);
+    }
+  });
+  return filelist;
+};
+
+// Requires a trailing slash on the dir argument because walk_sync does
+var js_application_files_in_dir = function(dir) {
+  return walk_sync_path(dir).filter(function(filename) {
+    return /\.js$/.test(filename) && !/_itest\.js$/.test(filename) && !/_utest\.js$/.test(filename);
+  });
+};
+
+var num_module_func = function(require_path) {
+  return _.functions(require(require_path)).length;
+};
+
+describe('exported functions - util', function() {
+  /**
+   * Map of module name to the number of functions that module exports
+   * @type {Object}
+   */
+  var f_exported_cnt = {};
+
+  it('app/util/logger/index', function() {
+    var name = 'app/util/logger/index';
+    var module = require(name);
+    f_exported_cnt[name] = 0;
+    module.get_logger.should.be.a.function;
+    f_exported_cnt[name]++;
+    module.get_log4js_logger.should.be.a.function;
+    f_exported_cnt[name]++;
+  });
+
+  /** Sequelize object has .sq object field for Sequelize and a .pr object field for all the models - no funcs */
+  it('app/util/pr/index', function() {
+    var name = 'app/util/pr/index';
+    var module = require(name);
+    f_exported_cnt[name] = 0;
+  });
+
+
+  /** Sequelize model module export is a function, not an object with functions - rely on Sequelize tests - no funcs */
+  it('app/util/pr/entry', function() {
+    var name = 'app/util/pr/entry';
+    var module = require(name);
+    f_exported_cnt[name] = 0;
+  });
+
+  // This test must be last in its suite
+  it('exports the expected functions', function() {
+    var js_files = js_application_files_in_dir('./src/server/app/util/').filter(function(filename) {
+      return !/.*\/router.js/.test(filename);
+    });
+    js_files.forEach(function(js_file) {
+      var req_path = js_file.replace(/\.\/src\/server\//, '').replace(/\.js$/, '');
+      var module = require(req_path);
+      should(f_exported_cnt[req_path]).not.be.type('undefined', req_path + ' exports not checked') &&
+        f_exported_cnt[req_path].should.equal(num_module_func(req_path),
+          req_path + ' only ' + f_exported_cnt[req_path] + ' of ' + num_module_func(req_path) + ' functions checked');
+    });
+  });
+});
