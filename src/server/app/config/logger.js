cacf357f756fc288298620be4285b84334bdedbb
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-04-03 17:54:47 +0100

    src/server/app/config/logger.js - comment clarification for future reference

--- src/server/app/config/logger.js
+++ src/server/app/config/logger.js
@@ -4,13 +4,6 @@
  * Logger-specific configuration
  */
 module.exports = {
-  /*
-  TODO: Modify log4js/lib/connect-logger to take option.tokens { key: func }, internally it should use a map
-        of tokens to functions (currently it is hard-coded in to the format method); require moment for method
-  TODO: Modify express format to use new jdate token, not date
-  express_tokens: {
-    jdate: function(req, res) { return moment().format('YYYY-MM-DD HH:mm:ss.SSS'); }
-  },
-  */
+  // TODO: When log4js version updated to include my PR, add express_tokens and override :date with custom token
   express_format: '[:date] :remote-addr ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"'
 };
