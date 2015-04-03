c4f84ceea088f4831a5b78dc8d7bbdfb43eff5fa
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-04-03 13:00:55 +0100

    Gruntfile.js - 4 space indent to 2 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,191 +1,190 @@
 'use strict';
 
 module.exports = function(grunt) {
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        // Configuration files and variables
-        pkg: grunt.file.readJSON('package.json'),
-        time: grunt.template.today("yyyymmdd-HHMM"),
-        today: grunt.template.today('yyyy-mm-dd'),
-        year: grunt.template.today('yyyy'),
+  grunt.initConfig({
+    // Configuration files and variables
+    pkg: grunt.file.readJSON('package.json'),
+    time: grunt.template.today("yyyymmdd-HHMM"),
+    today: grunt.template.today('yyyy-mm-dd'),
+    year: grunt.template.today('yyyy'),
 
-        // Plugin tasks
-        clean: {
-            all: {
-                src: ['build', 'test/out', 'test/tmp', '**/*~', '**/.*~']
-            }
+    // Plugin tasks
+    clean: {
+      all: {
+        src: ['build', 'test/out', 'test/tmp', '**/*~', '**/.*~']
+      }
+    },
+
+    // TODO Update jshint options to match code directory structure
+    jshint: {
+      all: [
+        'src/**/*.js',
+        '!src/server/node_modules/**/*.js',
+        '!src/client/bower_components/**/*.js',
+        '!src/test/blanket.js'
+
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    mochaTest: {
+      unit: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true,
+          require: 'src/test/blanket'
         },
-
-        // TODO Update jshint options to match code directory structure
-        jshint: {
-            all: [
-                'src/**/*.js',
-                '!src/server/node_modules/**/*.js',
-                '!src/client/bower_components/**/*.js',
-                '!src/client/node_modules/**/*.js',
-                '!src/test/blanket.js'
-
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-            },
+        src: [
+          './**/*_utest.js',
+          '!./test/blanket.js',
+          '!./test/out/server/node_modules/**/*.js',
+          '!./test/out/client/bower_components/**/*.js'
+        ]
+      },
+      integration: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true,
+          require: 'src/test/blanket'
         },
+        src: [
+          './**/*_itest.js',
+          '!./test/blanket.js',
+          '!./test/out/server/node_modules/**/*.js',
+          '!./test/out/client/bower_components/**/*.js'
+        ]
+      }
+    },
 
-        mochaTest: {
-            unit: {
-                options: {
-                    reporter: 'spec',
-                    clearRequireCache: true,
-                    require: 'src/test/blanket'
-                },
-                src: [
-                    './**/*_utest.js',
-                    '!./test/blanket.js',
-                    '!./test/out/server/node_modules/**/*.js',
-                    '!./test/out/client/bower_components/**/*.js'
-                ]
-            },
-            integration: {
-                options: {
-                    reporter: 'spec',
-                    clearRequireCache: true,
-                    require: 'src/test/blanket'
-                },
-                src: [
-                    './**/*_itest.js',
-                    '!./test/blanket.js',
-                    '!./test/out/server/node_modules/**/*.js',
-                    '!./test/out/client/bower_components/**/*.js'
-                ]
-            }
-        },
+    // POTENTIAL ISSUE: Does grunt-contrib-copy/tasks/copy.js use mode or just default it to false?
+    copy: {
+      build: {
+        cwd: 'src',
+        src: ['**'],
+        dest: 'build/out',
+        expand: true,
+        mode: true
+      },
+      test: {
+        cwd: 'src',
+        src: ['**'],
+        dest: 'test/out',
+        expand: true,
+        mode: true
+      }
+    },
 
-        // POTENTIAL ISSUE: Does grunt-contrib-copy/tasks/copy.js use mode or just default it to false?
-        copy: {
-            build: {
-                cwd: 'src',
-                src: ['**'],
-                dest: 'build/out',
-                expand: true,
-                mode: true
-            },
-            test: {
-                cwd: 'src',
-                src: ['**'],
-                dest: 'test/out',
-                expand: true,
-                mode: true
-            }
-        },
-
-        mkdir: {
-            build: {
-                options: {
-                    create: ['build/out/server/logs']
-                }
-            },
-            test: {
-                options: {
-                    create: ['test/out/server/logs']
-                }
-            }
-        },
-
-        usebanner: {
-            js: {
-                options: {
-                    banner: '/*\n' +
-                        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
-                        ' * <%= pkg.description %>\n' +
-                        ' * (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
-                        ' */\n'
-                },
-                files: {
-                    src: ['build/out/**/*.js', 'test/out/**/*.js']
-                }
-            },
-
-            css: {
-                options: {
-                    banner: '/*\n' +
-                        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
-                        ' * <%= pkg.description %>\n' +
-                        ' * (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
-                        ' */\n'
-                },
-                files: {
-                    src: ['build/out/**/*.css', 'test/out/**/*.css']
-                }
-            },
-
-            html: {
-                options: {
-                    banner: '<!--\n' +
-                        ' <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
-                        ' <%= pkg.description %>\n' +
-                        ' (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
-                        '-->\n'
-                },
-                files: {
-                    src: ['build/out/**/*.html', 'test/out/**/*.html']
-                }
-            },
-
-            sh: {
-                options: {
-                    banner: '#!/bin/bash\n' +
-                        '# <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
-                        '# <%= pkg.description %>\n' +
-                        '# (C) 2015-<%= year %> <%= pkg.author.name %>\n'
-                },
-                files: {
-                    src: ['build/out/**/*.sh', 'test/out/**/*.sh']
-                }
-            }
-        },
-
-        'git-describe': {
-            'options': {
-                'failOnError': true
-            },
-            'main': {}
-        },
-
-        compress: {
-            main: {
-                options: {
-                    archive: 'build/dist/postgres-fiddle-<%= pkg.version %>-<%= time %>.tar.gz',
-                    mode: 'tgz',
-                    pretty: true
-                },
-                files: [{
-                    cwd: 'build/out',
-                    expand: true,
-                    src: ['**'],
-                    dest: 'postgres-fiddle/'
-                }]
-            }
-        },
-
-        version_file: {
-            main: {
-                options: {
-                    out: 'build/out/version.json',
-                    generator_list: ['datestring', 'npm_version', 'git_describe'],
-                    generator_dir: 'generators'
-                }
-            }
+    mkdir: {
+      build: {
+        options: {
+          create: ['build/out/server/logs']
         }
-    });
+      },
+      test: {
+        options: {
+          create: ['test/out/server/logs']
+        }
+      }
+    },
 
-    grunt.registerTask('assemble', ['copy', 'mkdir']);
-    grunt.registerTask('metadata', ['usebanner', 'version_file']);
-    grunt.registerTask('test', ['jshint', 'mochaTest']);
+    usebanner: {
+      js: {
+        options: {
+          banner: '/*\n' +
+            ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
+            ' */\n'
+        },
+        files: {
+          src: ['build/out/**/*.js', 'test/out/**/*.js']
+        }
+      },
 
-    grunt.registerTask('build', ['clean', 'assemble', 'metadata', 'compress', 'test']);
-    grunt.registerTask('quick', ['clean', 'assemble', 'test']);
+      css: {
+        options: {
+          banner: '/*\n' +
+            ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
+            ' */\n'
+        },
+        files: {
+          src: ['build/out/**/*.css', 'test/out/**/*.css']
+        }
+      },
 
-    // Running test before compress does not work - why?
-    grunt.registerTask('broken_build', ['clean', 'assemble', 'test', 'compress'])
+      html: {
+        options: {
+          banner: '<!--\n' +
+            ' <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
+            ' <%= pkg.description %>\n' +
+            ' (C) 2015-<%= year %> <%= pkg.author.name %>\n' +
+            '-->\n'
+        },
+        files: {
+          src: ['build/out/**/*.html', 'test/out/**/*.html']
+        }
+      },
+
+      sh: {
+        options: {
+          banner: '#!/bin/bash\n' +
+            '# <%= pkg.name %> - v<%= pkg.version %> - <%= today %>\n' +
+            '# <%= pkg.description %>\n' +
+            '# (C) 2015-<%= year %> <%= pkg.author.name %>\n'
+        },
+        files: {
+          src: ['build/out/**/*.sh', 'test/out/**/*.sh']
+        }
+      }
+    },
+
+    'git-describe': {
+      'options': {
+        'failOnError': true
+      },
+      'main': {}
+    },
+
+    compress: {
+      main: {
+        options: {
+          archive: 'build/dist/postgres-fiddle-<%= pkg.version %>-<%= time %>.tar.gz',
+          mode: 'tgz',
+          pretty: true
+        },
+        files: [{
+          cwd: 'build/out',
+          expand: true,
+          src: ['**'],
+          dest: 'postgres-fiddle/'
+        }]
+      }
+    },
+
+    version_file: {
+      main: {
+        options: {
+          out: 'build/out/version.json',
+          generator_list: ['datestring', 'npm_version', 'git_describe'],
+          generator_dir: 'generators'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('assemble', ['copy', 'mkdir']);
+  grunt.registerTask('metadata', ['usebanner', 'version_file']);
+  grunt.registerTask('test', ['jshint', 'mochaTest']);
+
+  grunt.registerTask('build', ['clean', 'assemble', 'metadata', 'compress', 'test']);
+  grunt.registerTask('quick', ['clean', 'assemble', 'test']);
+
+  // Running test before compress does not work - why?
+  grunt.registerTask('broken_build', ['clean', 'assemble', 'test', 'compress'])
 };
